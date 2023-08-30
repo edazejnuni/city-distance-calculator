6a5ab405de7aebeab3cc9fc43476a021c0089b17
ezejnuni97 <112702287+ezejnuni97@users.noreply.github.com>
2023-08-30 22:08:26 +0200

    fix

--- src/utils/cityCoordinatesApi.js
+++ src/utils/cityCoordinatesApi.js
@@ -1,6 +1,6 @@
 export async function fetchCityCoordinates(cityNames) {
   try {
-    const response = await fetch(`https://cfbc-2a02-dd07-8000-6700-85a7-5264-a56c-3c1d.ngrok-free.app/searchCities`, {
+    const response = await fetch(`https://ae89-2a02-dd07-8000-6700-85a7-5264-a56c-3c1d.ngrok-free.app/searchCities`, {
       method: 'POST',
       body: JSON.stringify({ cityNames }),
       headers: {
